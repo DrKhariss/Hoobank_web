f4459cdb2d5805688e6ce1595d75f68c3ce3e1b5
Kharis Nkemena <chukwuebukankemena@gmail.com>
2025-08-06 11:38:15 +0100

    initial commit for bank_app project

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import styles from "./styles";
+import{ Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
+
+const App = () => {
+  return (
+    <>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.flexstart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero/>
+      </div>
+    
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexstart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats/>
+        <Business/>
+        <Billing/>
+        <CardDeal/>
+        <Testimonials/>
+        <Clients/>
+        <CTA/>
+        <Footer/>
+      </div>
+      </div>
+    
+    
+    
+    </> 
+  )
+}
+
+export default App
